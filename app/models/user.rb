56d726a8d14abe6a60df04f8b19114c364f679ba
CT Wu <ct@shoplineapp.com>
2022-07-06 17:03:43 +0800

    chore: from sqlite to mongodb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
-class User < ApplicationRecord
+class User
+    include Mongoid::Document
+    field :first_name, type: String
+    field :last_name, type: String
+    field :gender, type: String
+    field :age, type: Integer
+
     validates :first_name, presence: true, length: { minimum: 1 }
     validates :last_name, presence: true, length: { minimum: 1 }
     GENDER = ["male", "female", "others"]
     validates_inclusion_of :gender, :in => GENDER
-    validates :age, presence: true, numericality: { only_integer: true }
+    validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
