55fca06899d60256345c9d5ec5f14a2aa51a8b3f
CT Wu <ct@shoplineapp.com>
2022-07-07 14:03:31 +0800

    test: add user model ut

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+class AddressValidator < ActiveModel::Validator
+    def validate(record)
+        begin
+            record.address.assert_valid_keys(:country, :address_1, :address_2) if record.address
+        rescue ArgumentError
+            record.errors[:base] << "Address is invalid"
+        end
+    end
+end
+
 class User
     include Mongoid::Document
     include Mongoid::Timestamps
@@ -13,6 +23,8 @@ class User
     GENDER = ["male", "female", "others"]
     validates_inclusion_of :gender, :in => GENDER
     validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+    validates_with AddressValidator
 
     has_one :shop
 end
---
+++ spec/models/users_model_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe ".validate" do
+
+    it "is valid" do
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 25, :gender => 'male')
+      expect( user ).to be_valid
+      
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 25, :gender => 'female',
+                      :address => {:country => 'United States'})
+      expect( user ).to be_valid
+      
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 25, :gender => 'female',
+                      :address => {:country => 'United States', :address_1 => 'United States', :address_2 => 'United States'})
+      expect( user ).to be_valid
+    end
+
+    it "is invalid without name" do
+      user = User.new(:first_name => '', :last_name => 'Doe', :age => 25, :gender => 'male')
+      expect( user ).to_not be_valid
+      
+      user = User.new(:first_name => 'John', :last_name => '', :age => 25, :gender => 'male')
+      expect( user ).to_not be_valid
+    end
+
+    it "is invalid with incorrect age" do
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 0, :gender => 'male')
+      expect( user ).to_not be_valid
+      
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => -10, :gender => 'male')
+      expect( user ).to_not be_valid
+      
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :gender => 'male')
+      expect( user ).to_not be_valid
+    end
+
+    it "is invalid with incorrect gender" do
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 25, :gender => 'maled')
+      expect( user ).to_not be_valid
+    end
+    
+    it "is invalid with incorrect address" do
+      user = User.new(:first_name => 'John', :last_name => 'Doe', :age => 25, :gender => 'male',
+                      :address => {:address_3 => '123'})
+      expect( user ).to_not be_valid
+    end
+  end
+end
